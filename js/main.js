28b71801199eb8c339fdf19a86341bc0187735e6
GSergiy13 <frodo13lot@gmail.com>
2020-12-18 14:09:24 +0200

    Контент-мейкер - deniskaplunov

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(function(){
+	$('.slider_slick').slick({
+		nextArrow:'<button class="slick-arrow slick-next"><img src="images/button.png" alt="next"></button>',
+        prevArrow:'<button class="slick-arrow slick-prev"><img src="images/button.png" alt="prev"></button>',
+        autoplay: true,
+		responsive: [
+		{
+			breakpoint: 661,
+			settings: {
+				arrows:false
+			}
+		}
+		]
+	});
+});
+
+new WOW().init();
+
+$(".header__content, .about").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+
+
+    // Timer
+
+    const deadline = '2021-01-15';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
